2296c8c789ce20f1e5af65996e67b305b681aa3a
Caio Bertaglia <125330644+CaioBertaglia@users.noreply.github.com>
2023-10-06 02:39:59 -0300

    Add files via upload

---
+++ python/src/operadores.py
@@ -0,0 +1,62 @@
+# Exemplos de operadores aritméticos
+a = 10
+b = 5
+
+soma = a + b
+subtracao = a - b
+multiplicacao = a * b
+divisao = a / b
+divisao_inteira = a // b
+modulo = a % b
+exponenciacao = a ** b
+
+print("Soma:", soma)
+print("Subtração:", subtracao)
+print("Multiplicação:", multiplicacao)
+print("Divisão:", divisao)
+print("Divisão Inteira:", divisao_inteira)
+print("Módulo:", modulo)
+print("Exponenciação:", exponenciacao)
+
+# Exemplos de operadores de comparação
+x = 10
+y = 5
+
+igual = x == y
+diferente = x != y
+maior_que = x > y
+menor_que = x < y
+maior_ou_igual = x >= y
+menor_ou_igual = x <= y
+
+print("Igual:", igual)
+print("Diferente:", diferente)
+print("Maior que:", maior_que)
+print("Menor que:", menor_que)
+print("Maior ou igual que:", maior_ou_igual)
+print("Menor ou igual que:", menor_ou_igual)
+
+# Exemplos de operadores lógicos
+p = True
+q = False
+
+e_logico = p and q
+ou_logico = p or q
+nao_logico = not p
+
+print("E lógico:", e_logico)
+print("OU lógico:", ou_logico)
+print("NÃO lógico:", nao_logico)
+
+# Exemplos de operadores de atribuição
+x = 10
+
+x += 5  # x = x + 5
+x -= 3  # x = x - 3
+x *= 2  # x = x * 2
+x /= 4  # x = x / 4
+x //= 3  # x = x // 3
+x %= 2  # x = x % 2
+x **= 3  # x = x ** 3
+
+print("Valor final de x:", x)
